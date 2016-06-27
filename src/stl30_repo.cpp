49f2e38f46027f7a7c10e41c409f259fc3d43ea4
YanShenChun <shanghaitan1987@126.com>
2016-06-27 18:08:23 +0800

    Add the sgi_stl 3.0 source code.

---
+++ src/stl30_repo.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : stl30_repo.cpp
+// Author      : ted
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <map>
+
+int main() {
+	std::cout << "using SGI STL map .." << std::endl;
+	stdd::map<int, int> sgi_map;
+	return 0;
+}
